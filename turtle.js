e4d549f35b4cb46dfcaab6532786a6daf436c17e
spurekarcareer <spurekarcareer@users.noreply.github.com>
2013-10-30 14:08:18 -0400

    Turtle draws lines now

--- turtle.js
+++ turtle.js
@@ -1,6 +1,5 @@
 ;
 
-turnflag = false;
 deg = 0;
 
 function main() {
@@ -13,21 +12,30 @@ function init() {
     H = window.innerHeight-100;
     W = window.innerWidth-100;
 
+    //Set up canvas
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
     canvas.height = H;
     canvas.width = H;
     ctx.fillStyle = "#2E2E2E";
     ctx.fillRect(0,0,H,H);
-
+    
+    //Create player
     player = new Player();
 
+    //Set up path drawing
+    ctx.beginPath();
+    ctx.moveTo(player.xpos,player.ypos);
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = 2;
+
+    //Capture key presses
     addEventListener('keydown', keypress, false);
 };
 
 function gameLoop() {
     draw();
-    setTimeout(gameLoop, 100);
+    setTimeout(gameLoop, 25);
 };
 
 function draw() {
@@ -35,11 +43,18 @@ function draw() {
     ctx.fillStyle = "#2E2E2E";
     ctx.fillRect(0,0,canvas.width, canvas.height);
     ctx.translate(player.xpos,player.ypos);
+   
+    //handle turns by rotating canvas
     ctx.rotate(deg*Math.PI/180);
     deg = 0;
     ctx.translate(-player.xpos,-player.ypos);
 
+    //draw turtle
     player.draw();
+
+    //draw path
+    ctx.lineTo(player.xpos,player.ypos);
+    ctx.stroke();
 
     ctx.restore();
 };
@@ -87,7 +102,6 @@ function keypress(e) { //e is event given by listener
             player.moveReverse(20);
             break;
         case 32: //spacebar
-            turnflag = true;
             deg += 90;
             console.log("turn" + deg);
             break;
